102e6a3bc1b7ab22f34a44e30edfb9be7de04083
Hannes Sachsenhofer <hannes.sachsenhofer@adliance.net>
2020-11-19 08:35:42 +0100

    Add new abstraction for storage.

---
+++ src/Adliance.AspNetCore.Buddy.Abstractions/IStorage.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace Adliance.AspNetCore.Buddy.Abstractions
+{
+    public interface IStorage
+    {
+        Task Save(byte[] bytes, params string[] path);
+        Task Save(Stream stream, params string[] path);
+        
+        Task<byte[]> Load(params string[] path);
+        Task Load(Stream stream, params string[] path);
+        
+        Task<bool> Exists(params string[] path);
+        Task Delete(params string[] path);
+    }
+}
