10ec5f6c1519a1accc3c959ec5526e85d83f3867
Hannes Sachsenhofer <hannes.sachsenhofer@adliance.net>
2021-04-02 16:55:59 +0200

    Fix unit tests.

--- test/Adliance.AspNetCore.Buddy.Storage.Test/MockedAdliancePdferSettings.cs
+++ test/Adliance.AspNetCore.Buddy.Storage.Test/MockedAdliancePdferSettings.cs
@@ -10,6 +10,8 @@ namespace Adliance.AspNetCore.Buddy.Storage.Test
 
         public string AzureStorageConnectionString => Environment.GetEnvironmentVariable("Adliance_Buddy_Tests__AzureStorageConnectionString") ?? "";
         public bool AutomaticallyCreateDirectories => true;
+        public bool ConfigureDataProtection => true;
+        public string DataProtectionContainer => "dataprotection";
         public bool UseAzureStorage => false;
         public bool UseLocalStorage => false;
     }
