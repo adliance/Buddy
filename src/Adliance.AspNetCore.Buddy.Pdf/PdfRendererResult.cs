06923fabe1d65a7f1bcd898f3502992b579c0419
Adrian Fürschuß <adrian.fuerschuss@adliance.net>
2021-07-08 16:46:49 +0200

    Add readme for PDF project.

--- src/Adliance.AspNetCore.Buddy.Pdf/PdfRendererResult.cs
+++ src/Adliance.AspNetCore.Buddy.Pdf/PdfRendererResult.cs
@@ -4,8 +4,8 @@ namespace Adliance.AspNetCore.Buddy.Pdf
 {
     public class PdfRendererResult : IEmailAttachment
     {
-        public string Html { get; set; } = "";
-        public string Filename { get; set; } = "";
+        public string Html { get; set; } = string.Empty;
+        public string Filename { get; set; } = string.Empty;
         public byte[] Bytes { get; set; } = new byte[0];
     }
 }
--- src/Adliance.AspNetCore.Buddy.Pdf/V2/PdfOptions.cs
+++ src/Adliance.AspNetCore.Buddy.Pdf/V2/PdfOptions.cs
@@ -2,9 +2,24 @@
 {
     public class PdfOptions
     {
+        /// <summary>
+        /// The HTML for the PDF header as string.
+        /// </summary>
         public string? HeaderHtml { get; set; } = null;
+        
+        /// <summary>
+        /// The height of the header in pixel (px). If a HeaderHtml is provided, the height must be set.
+        /// </summary>
         public int? HeaderHeight { get; set; } = null;
+        
+        /// <summary>
+        /// The HTML for the PDF header as string.
+        /// </summary>
         public string? FooterHtml { get; set; } = null;
+        
+        /// <summary>
+        /// The height of the footer in pixel (px). If a FooterHtml is provided, the height must be set.
+        /// </summary>
         public int? FooterHeight { get; set; } = null;
     }
 }
