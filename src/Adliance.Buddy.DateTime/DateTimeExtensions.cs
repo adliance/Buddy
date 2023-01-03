a6ad6085b4d0fd28df53ea3d9f373dd8a554a642
Hannes Sachsenhofer <hannes.sachsenhofer@adliance.net>
2023-01-03 13:59:53 +0100

    Fix namespace.

--- src/Adliance.Buddy.DateTime/DateTimeExtensions.cs
+++ src/Adliance.Buddy.DateTime/DateTimeExtensions.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using System.Globalization;
+﻿using System.Globalization;
 using NodaTime;
 
-namespace Adliance.AspNetCore.Buddy.Extensions;
+namespace Adliance.Buddy.DateTime;
 
 public static class DateTimeExtensions
 {
@@ -10,25 +9,25 @@ public static class DateTimeExtensions
     private const string DefaultDateFormat = "dd. MMMM yyyy";
     private const string DefaultDateTimeFormat = "dd. MMMM yyyy, HH:mm:ss";
 
-    public static DateTime? UtcToCet(this DateTime? utcDateTime)
+    public static System.DateTime? UtcToCet(this System.DateTime? utcDateTime)
     {
         if (utcDateTime.HasValue) return UtcToCet(utcDateTime.Value);
         return null;
     }
 
-    public static DateTime UtcToCet(this DateTime utcDateTime)
+    public static System.DateTime UtcToCet(this System.DateTime utcDateTime)
     {
         var vienna = DateTimeZoneProviders.Tzdb["Europe/Vienna"];
         return LocalDateTime.FromDateTime(utcDateTime).InZoneStrictly(DateTimeZone.Utc).WithZone(vienna).ToDateTimeUnspecified();
     }
 
-    public static DateTime? CetToUtc(this DateTime? cetDateTime)
+    public static System.DateTime? CetToUtc(this System.DateTime? cetDateTime)
     {
         if (cetDateTime.HasValue) return CetToUtc(cetDateTime.Value);
         return null;
     }
 
-    public static DateTime CetToUtc(this DateTime cetDateTime)
+    public static System.DateTime CetToUtc(this System.DateTime cetDateTime)
     {
         var vienna = DateTimeZoneProviders.Tzdb["Europe/Vienna"];
         return LocalDateTime.FromDateTime(cetDateTime).InZoneStrictly(vienna).ToDateTimeUtc();
@@ -36,32 +35,32 @@ public static class DateTimeExtensions
 
     #region Format Date
 
-    public static string FormatDate(this DateTime? dateTime, string format = DefaultDateFormat, string culture = DefaultCulture)
+    public static string FormatDate(this System.DateTime? dateTime, string format = DefaultDateFormat, string culture = DefaultCulture)
     {
         return dateTime.HasValue ? FormatDate(dateTime.Value, format, new CultureInfo(culture)) : "";
     }
 
-    public static string FormatUtcAsCetDate(this DateTime? dateTime, string format = DefaultDateFormat, string culture = DefaultCulture)
+    public static string FormatUtcAsCetDate(this System.DateTime? dateTime, string format = DefaultDateFormat, string culture = DefaultCulture)
     {
         return dateTime.HasValue ? FormatUtcAsCetDate(dateTime.Value, format, new CultureInfo(culture)) : "";
     }
 
-    public static string FormatDate(this DateTime dateTime, string format = DefaultDateFormat, string culture = DefaultCulture)
+    public static string FormatDate(this System.DateTime dateTime, string format = DefaultDateFormat, string culture = DefaultCulture)
     {
         return FormatDate(dateTime, format, new CultureInfo(culture));
     }
 
-    public static string FormatUtcAsCetDate(this DateTime dateTime, string format = DefaultDateFormat, string culture = DefaultCulture)
+    public static string FormatUtcAsCetDate(this System.DateTime dateTime, string format = DefaultDateFormat, string culture = DefaultCulture)
     {
         return FormatUtcAsCetDate(dateTime, format, new CultureInfo(culture));
     }
 
-    public static string FormatDate(this DateTime dateTime, string format, CultureInfo culture)
+    public static string FormatDate(this System.DateTime dateTime, string format, CultureInfo culture)
     {
         return dateTime == default ? "" : dateTime.ToString(format, culture);
     }
 
-    public static string FormatUtcAsCetDate(this DateTime dateTime, string format, CultureInfo culture)
+    public static string FormatUtcAsCetDate(this System.DateTime dateTime, string format, CultureInfo culture)
     {
         return FormatDate(dateTime.UtcToCet(), format, culture);
     }
@@ -70,32 +69,32 @@ public static class DateTimeExtensions
 
     #region Format Date Time
 
-    public static string FormatDateTime(this DateTime? dateTime, string format = DefaultDateTimeFormat, string culture = DefaultCulture)
+    public static string FormatDateTime(this System.DateTime? dateTime, string format = DefaultDateTimeFormat, string culture = DefaultCulture)
     {
         return dateTime.HasValue ? FormatDateTime(dateTime.Value, format, new CultureInfo(culture)) : "";
     }
 
-    public static string FormatUtcAsCetDateTime(this DateTime? dateTime, string format = DefaultDateTimeFormat, string culture = DefaultCulture)
+    public static string FormatUtcAsCetDateTime(this System.DateTime? dateTime, string format = DefaultDateTimeFormat, string culture = DefaultCulture)
     {
         return dateTime.HasValue ? FormatUtcAsCetDateTime(dateTime.Value, format, new CultureInfo(culture)) : "";
     }
 
-    public static string FormatDateTime(this DateTime dateTime, string format = DefaultDateTimeFormat, string culture = DefaultCulture)
+    public static string FormatDateTime(this System.DateTime dateTime, string format = DefaultDateTimeFormat, string culture = DefaultCulture)
     {
         return FormatDateTime(dateTime, format, new CultureInfo(culture));
     }
 
-    public static string FormatUtcAsCetDateTime(this DateTime dateTime, string format = DefaultDateTimeFormat, string culture = DefaultCulture)
+    public static string FormatUtcAsCetDateTime(this System.DateTime dateTime, string format = DefaultDateTimeFormat, string culture = DefaultCulture)
     {
         return FormatUtcAsCetDateTime(dateTime, format, new CultureInfo(culture));
     }
 
-    public static string FormatDateTime(this DateTime dateTime, string format, CultureInfo culture)
+    public static string FormatDateTime(this System.DateTime dateTime, string format, CultureInfo culture)
     {
         return dateTime == default ? "" : dateTime.ToString(format, culture);
     }
 
-    public static string FormatUtcAsCetDateTime(this DateTime dateTime, string format, CultureInfo culture)
+    public static string FormatUtcAsCetDateTime(this System.DateTime dateTime, string format, CultureInfo culture)
     {
         return FormatDateTime(dateTime.UtcToCet(), format, culture);
     }
--- test/Adliance.Buddy.DateTime.Test/Extensions/DateTimeExtensionsTest.cs
+++ test/Adliance.Buddy.DateTime.Test/Extensions/DateTimeExtensionsTest.cs
@@ -1,7 +1,6 @@
-using Adliance.AspNetCore.Buddy.Extensions;
 using Xunit;
 
-namespace Adliance.AspNetCore.Buddy.Test.Extensions;
+namespace Adliance.Buddy.DateTime.Test.Extensions;
 
 public class DateTimeExtensionsTest
 {
@@ -10,13 +9,13 @@ public class DateTimeExtensionsTest
     [InlineData("2022-12-18 12:34:56", "2022-12-18 13:34:56")]
     public void Can_Convert_Utc_To_Cet(string originalDate, string expectedDate)
     {
-        Assert.Equal(DateTime.Parse(expectedDate), DateTime.Parse(originalDate).UtcToCet());
+        Assert.Equal(System.DateTime.Parse(expectedDate), System.DateTime.Parse(originalDate).UtcToCet());
     }
 
     [Fact]
     public void Can_Support_Null_When_Converting_Utc_To_Cet()
     {
-        Assert.Null((null as DateTime?).UtcToCet());
+        Assert.Null((null as System.DateTime?).UtcToCet());
     }
     
     [Theory]
@@ -24,13 +23,13 @@ public class DateTimeExtensionsTest
     [InlineData("2022-12-18 12:34:56", "2022-12-18 11:34:56")]
     public void Can_Convert_Cet_To_Utc(string originalDate, string expectedDate)
     {
-        Assert.Equal(DateTime.Parse(expectedDate), DateTime.Parse(originalDate).CetToUtc());
+        Assert.Equal(System.DateTime.Parse(expectedDate), System.DateTime.Parse(originalDate).CetToUtc());
     }
 
     [Fact]
     public void Can_Support_Null_When_Converting_Cet_To_Utc()
     {
-        Assert.Null((null as DateTime?).CetToUtc());
+        Assert.Null((null as System.DateTime?).CetToUtc());
     }
 
     [Theory]
@@ -38,13 +37,13 @@ public class DateTimeExtensionsTest
     [InlineData("2022-12-18 12:34:56", "18. Dezember 2022")]
     public void Can_Format_Date(string date, string expectedResult)
     {
-        Assert.Equal(expectedResult, DateTime.Parse(date).FormatDate());
+        Assert.Equal(expectedResult, System.DateTime.Parse(date).FormatDate());
     }
     
     [Fact]
     public void Can_Support_Null_When_Formatting_Date()
     {
-        Assert.Equal("", (null as DateTime?).FormatDate());
+        Assert.Equal("", (null as System.DateTime?).FormatDate());
     }
     
     [Theory]
@@ -52,13 +51,13 @@ public class DateTimeExtensionsTest
     [InlineData("2022-12-18 23:45:56", "19. Dezember 2022")]
     public void Can_Format_Utc_Date(string date, string expectedResult)
     {
-        Assert.Equal(expectedResult, DateTime.Parse(date).FormatUtcAsCetDate());
+        Assert.Equal(expectedResult, System.DateTime.Parse(date).FormatUtcAsCetDate());
     }
     
     [Fact]
     public void Can_Support_Null_When_Formatting_Utc_Date()
     {
-        Assert.Equal("", (null as DateTime?).FormatUtcAsCetDate());
+        Assert.Equal("", (null as System.DateTime?).FormatUtcAsCetDate());
     }
 
     [Theory]
@@ -66,13 +65,13 @@ public class DateTimeExtensionsTest
     [InlineData("2022-12-18 01:23:45", "18. Dezember 2022, 01:23:45")]
     public void Can_Format_DateTime(string date, string expectedResult)
     {
-        Assert.Equal(expectedResult, DateTime.Parse(date).FormatDateTime());
+        Assert.Equal(expectedResult, System.DateTime.Parse(date).FormatDateTime());
     }
     
     [Fact]
     public void Can_Support_Null_When_Formatting_DateTime()
     {
-        Assert.Equal("", (null as DateTime?).FormatDateTime());
+        Assert.Equal("", (null as System.DateTime?).FormatDateTime());
     }
     
     [Theory]
@@ -80,12 +79,12 @@ public class DateTimeExtensionsTest
     [InlineData("2022-12-18 23:45:56", "19. Dezember 2022, 00:45:56")]
     public void Can_Format_Utc_DateTime(string date, string expectedResult)
     {
-        Assert.Equal(expectedResult, DateTime.Parse(date).FormatUtcAsCetDateTime());
+        Assert.Equal(expectedResult, System.DateTime.Parse(date).FormatUtcAsCetDateTime());
     }
     
     [Fact]
     public void Can_Support_Null_When_Formatting_Utc_DateTime()
     {
-        Assert.Equal("", (null as DateTime?).FormatUtcAsCetDateTime());
+        Assert.Equal("", (null as System.DateTime?).FormatUtcAsCetDateTime());
     }
 }
