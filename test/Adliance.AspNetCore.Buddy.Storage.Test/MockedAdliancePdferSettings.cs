93a733d857a6e126895cf2d1b11dd4f7ffaf573e
Hannes Sachsenhofer <hannes.sachsenhofer@adliance.net>
2023-11-23 11:59:33 +0100

    Still fighting the pipeline.

--- test/Adliance.AspNetCore.Buddy.Storage.Test/MockedAdliancePdferSettings.cs
+++ test/Adliance.AspNetCore.Buddy.Storage.Test/MockedAdliancePdferSettings.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Text;
 
 namespace Adliance.AspNetCore.Buddy.Storage.Test
 {
@@ -8,21 +9,28 @@ namespace Adliance.AspNetCore.Buddy.Storage.Test
         public StorageType Type => StorageType.Local;
         public string LocalStorageBasePath => Path.GetTempPath();
 
-        public string AzureStorageConnectionString
-        {
-            get
-            {
-                const string envName = "Adliance_Buddy_Tests__AzureStorageConnectionString";
-                var envValue = Environment.GetEnvironmentVariable(envName);
-                if (string.IsNullOrWhiteSpace(envValue)) throw new Exception($"No storage connection string found at env variable \"{envName}\".");
-                return envValue;
-            }
-        }
+        public string AzureStorageConnectionString => GetEnvironmentVariable("Adliance_Buddy_Tests__AzureStorageConnectionString");
 
         public bool AutomaticallyCreateDirectories => true;
         public bool ConfigureDataProtection => true;
         public string DataProtectionContainer => "dataprotection";
         public bool UseAzureStorage => false;
         public bool UseLocalStorage => false;
+        
+        public string GetEnvironmentVariable(string name)
+        {
+            return Environment.GetEnvironmentVariable(name)
+                   ?? Environment.GetEnvironmentVariable(name.ToUpper())
+                   ?? Environment.GetEnvironmentVariable(name.ToLower())
+                   ?? throw BuildEnvironmentVariableException(name);
+        }
+
+        private Exception BuildEnvironmentVariableException(string name)
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine($"Environment variable \"{name}\" missing. Available environment variables are:");
+            foreach (var o in Environment.GetEnvironmentVariables()) sb.AppendLine(o.ToString());
+            throw new Exception(sb.ToString());
+        }
     }
 }
