355cd63d3c1131dd1b43d866bb3e55b8e95976a5
Hannes Sachsenhofer <hannes.sachsenhofer@adliance.net>
2023-11-23 09:46:01 +0100

    Try to fix pipeline to support env variable.

--- test/Adliance.AspNetCore.Buddy.Storage.Test/MockedAdliancePdferSettings.cs
+++ test/Adliance.AspNetCore.Buddy.Storage.Test/MockedAdliancePdferSettings.cs
@@ -8,7 +8,17 @@ namespace Adliance.AspNetCore.Buddy.Storage.Test
         public StorageType Type => StorageType.Local;
         public string LocalStorageBasePath => Path.GetTempPath();
 
-        public string AzureStorageConnectionString => Environment.GetEnvironmentVariable("Adliance_Buddy_Tests__AzureStorageConnectionString") ?? "";
+        public string AzureStorageConnectionString
+        {
+            get
+            {
+                const string envName = "Adliance_Buddy_Tests__AzureStorageConnectionString";
+                var envValue = Environment.GetEnvironmentVariable(envName);
+                if (string.IsNullOrWhiteSpace(envValue)) throw new Exception($"No storage connection string found at env variable \"{envName}\".");
+                return envValue;
+            }
+        }
+
         public bool AutomaticallyCreateDirectories => true;
         public bool ConfigureDataProtection => true;
         public string DataProtectionContainer => "dataprotection";
