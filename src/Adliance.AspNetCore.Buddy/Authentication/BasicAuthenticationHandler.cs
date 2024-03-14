c2107ceedc1646df48ea8393c04df69acafb4ed8
Daniel Morawetz <daniel.morawetz@adliance.net>
2024-03-14 16:34:28 +0100

    Bump Adliance.AspNetCore.Buddy to dotnet 8
    ISystemClock is deprecated, and superseded by TimeProvider. I removed the parameter from the header. The added unit test shows how to set the new TimeProvider in the AuthenticationSchemeOptions. cf. https://learn.microsoft.com/en-us/dotnet/core/compatibility/aspnet-core/8.0/isystemclock-obsolete

--- src/Adliance.AspNetCore.Buddy/Authentication/BasicAuthenticationHandler.cs
+++ src/Adliance.AspNetCore.Buddy/Authentication/BasicAuthenticationHandler.cs
@@ -1,73 +1,56 @@
-﻿using Microsoft.AspNetCore.Authentication;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
-using System;
+﻿using System;
 using System.Net.Http.Headers;
 using System.Security.Claims;
 using System.Text;
 using System.Text.Encodings.Web;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 
-namespace Adliance.AspNetCore.Buddy.Authentication
+namespace Adliance.AspNetCore.Buddy.Authentication;
+
+/// <inheritdoc />
+/// <remarks>Thanks to https://github.com/cornflourblue/aspnet-core-basic-authentication-api</remarks>
+public class BasicAuthenticationHandler(
+    IOptionsMonitor<BasicAuthenticationOptions> options,
+    ILoggerFactory logger,
+    UrlEncoder encoder,
+    IBasicAuthenticationService authenticationService)
+    : AuthenticationHandler<BasicAuthenticationOptions>(options, logger, encoder)
 {
-    /// <inheritdoc/>
-    /// <remarks>Thanks to https://github.com/cornflourblue/aspnet-core-basic-authentication-api</remarks>
-    public class BasicAuthenticationHandler : AuthenticationHandler<BasicAuthenticationOptions>
+    protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
     {
-        #region Constructor
+        if (!Request.Headers.ContainsKey("Authorization") || Request.Headers.Authorization.Count == 0)
+            return AuthenticateResult.Fail("Missing Authorization Header");
 
-        private readonly IBasicAuthenticationService _authenticationService;
-
-        public BasicAuthenticationHandler(
-            IOptionsMonitor<BasicAuthenticationOptions> options,
-            ILoggerFactory logger,
-            UrlEncoder encoder,
-            ISystemClock clock,
-            IBasicAuthenticationService authenticationService)
-            : base(options, logger, encoder, clock)
+        BasicAuthenticationUser? user;
+        try
         {
-            _authenticationService = authenticationService;
+            var authHeader = AuthenticationHeaderValue.Parse(Request.Headers.Authorization[0]!);
+            var credentialBytes = Convert.FromBase64String(authHeader.Parameter ?? "");
+            var credentials = Encoding.UTF8.GetString(credentialBytes).Split(':');
+            var username = credentials[0];
+            var password = credentials[1];
+            user = await authenticationService.AuthenticateAsync(username, password);
+        }
+        catch
+        {
+            return AuthenticateResult.Fail("Invalid Authorization Header");
         }
 
-        #endregion Constructor
+        // ReSharper disable once ConditionIsAlwaysTrueOrFalseAccordingToNullableAPIContract
+        if (user == null) return AuthenticateResult.Fail("Invalid Username or Password");
 
-        protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
+        var claims = new[]
         {
-            if (!Request.Headers.ContainsKey("Authorization"))
-            {
-                return AuthenticateResult.Fail("Missing Authorization Header");
-            }
+            new Claim(ClaimTypes.NameIdentifier, user.Id ?? ""),
+            new Claim(ClaimTypes.Name, user.Name ?? "")
+        };
+        var identity = new ClaimsIdentity(claims, Scheme.Name);
+        var principal = new ClaimsPrincipal(identity);
+        var ticket = new AuthenticationTicket(principal, Scheme.Name);
 
-            BasicAuthenticationUser? user = null;
-            try
-            {
-                var authHeader = AuthenticationHeaderValue.Parse(Request.Headers["Authorization"]);
-                var credentialBytes = Convert.FromBase64String(authHeader.Parameter ?? "");
-                var credentials = Encoding.UTF8.GetString(credentialBytes).Split(':');
-                var username = credentials[0];
-                var password = credentials[1];
-                user = await _authenticationService.AuthenticateAsync(username, password);
-            }
-            catch
-            {
-                return AuthenticateResult.Fail("Invalid Authorization Header");
-            }
-
-            // ReSharper disable once ConditionIsAlwaysTrueOrFalseAccordingToNullableAPIContract
-            if (user == null)
-            {
-                return AuthenticateResult.Fail("Invalid Username or Password");
-            }
-
-            var claims = new[] {
-                new Claim(ClaimTypes.NameIdentifier, user.Id ?? ""),
-                new Claim(ClaimTypes.Name, user.Name ?? ""),
-            };
-            var identity = new ClaimsIdentity(claims, Scheme.Name);
-            var principal = new ClaimsPrincipal(identity);
-            var ticket = new AuthenticationTicket(principal, Scheme.Name);
-
-            return AuthenticateResult.Success(ticket);
-        }
+        return AuthenticateResult.Success(ticket);
     }
 }
---
+++ test/Adliance.AspNetCore.Buddy.Test/BasicAuthenticationHandlerTest.cs
@@ -0,0 +1,92 @@
+using System.Text;
+using System.Text.Encodings.Web;
+using Adliance.AspNetCore.Buddy.Authentication;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.Extensions.Primitives;
+using Microsoft.Extensions.Time.Testing;
+using Microsoft.Net.Http.Headers;
+using Moq;
+using Xunit;
+
+namespace Adliance.AspNetCore.Buddy.Test;
+
+public class BasicAuthenticationHandlerTest
+{
+    private readonly BasicAuthenticationHandler _handler;
+
+    public BasicAuthenticationHandlerTest()
+    {
+        Mock<IOptionsMonitor<BasicAuthenticationOptions>> options = new();
+        var timeProvider = new FakeTimeProvider();
+        timeProvider.SetLocalTimeZone(TimeZoneInfo.Utc);
+        timeProvider.SetUtcNow(new DateTimeOffset(2024, 3, 14, 12, 0, 0, TimeSpan.Zero));
+
+        options
+            .Setup(x => x.Get(It.IsAny<string>()))
+            .Returns(new BasicAuthenticationOptions
+            {
+                // Time provider is superseding the ISystemClock
+                TimeProvider = timeProvider
+            });
+
+        var logger = new Mock<ILogger<BasicAuthenticationHandler>>();
+        Mock<ILoggerFactory> loggerFactory = new();
+        loggerFactory.Setup(x => x.CreateLogger(It.IsAny<string>())).Returns(logger.Object);
+
+        Mock<UrlEncoder> encoder = new();
+        Mock<IBasicAuthenticationService> basicAuthenticationService = new();
+        basicAuthenticationService
+            .Setup(x => x.AuthenticateAsync("username", "password"))
+            .ReturnsAsync(
+                new BasicAuthenticationUser
+                {
+                    Id = "test",
+                    Name = "Super Tester"
+                });
+
+        _handler = new BasicAuthenticationHandler(options.Object, loggerFactory.Object, encoder.Object,
+            basicAuthenticationService.Object);
+    }
+
+    [Fact]
+    public async Task HandleAuthenticateAsync_NoAuthorizationHeader_ReturnsAuthenticateResultFail()
+    {
+        var context = new DefaultHttpContext();
+        context.Request.Headers[HeaderNames.Authorization] = new StringValues(string.Empty);
+
+        await _handler.InitializeAsync(
+            new AuthenticationScheme(BasicAuthenticationDefaults.AuthenticationScheme, null,
+                typeof(BasicAuthenticationHandler)), context);
+        var result = await _handler.AuthenticateAsync();
+
+        Assert.False(result.Succeeded);
+        Assert.Equal("Invalid Authorization Header", result.Failure?.Message);
+    }
+
+    [Fact]
+    public async Task HandleAuthenticateAsync_ValidAuthorizationHeader_ReturnsAuthenticationTicket()
+    {
+        var context = new DefaultHttpContext();
+        var credentials = Encoding.UTF8.GetBytes("username:password");
+        var credentialsEncoded = Convert.ToBase64String(credentials);
+        context.Request.Headers[HeaderNames.Authorization] = new StringValues($"Basic {credentialsEncoded}");
+
+        await _handler.InitializeAsync(
+            new AuthenticationScheme(BasicAuthenticationDefaults.AuthenticationScheme, null,
+                typeof(BasicAuthenticationHandler)), context);
+        var result = await _handler.AuthenticateAsync();
+
+        Assert.True(result.Succeeded);
+        Assert.Null(result.Failure);
+        Assert.Equal("Super Tester", result.Principal?.Identity?.Name);
+        // Assert.Equal(new DateTime(new DateOnly(2024, 1, 2), new TimeOnly()), result.Ticket.Properties.ExpiresUtc);
+        Assert.Null(result.Ticket.Properties.ExpiresUtc);
+        Assert.Null(result.Ticket.Properties.IssuedUtc);
+        Assert.False(result.Ticket.Properties.IsPersistent);
+        Assert.Null(result.Ticket.Properties.AllowRefresh);
+        Assert.Equal("BasicAuthentication", result.Ticket.AuthenticationScheme);
+    }
+}
