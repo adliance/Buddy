98dd5de1f6a1a46bcd2c51b556e64c681b5afb30
Hannes Sachsenhofer <hannes.sachsenhofer@adliance.net>
2025-05-30 11:49:10 +0200

    Support test project in pipeline template for single project.

--- test/Adliance.AspNetCore.Buddy.Storage.Test/StorageTestBase.cs
+++ test/Adliance.AspNetCore.Buddy.Storage.Test/StorageTestBase.cs
@@ -139,9 +139,8 @@ public abstract class StorageTestBase
         await _storage.Save([1, 2, 3, 4], false, container, "file2.bin");
         await _storage.Save([1, 2, 3, 4, 5], false, container, "file3.bin");
         Assert.Equal(3, (await _storage.List(container)).Count);
-        Thread.Sleep(2000);
+        Thread.Sleep(1000);
         await _storage.Save([1, 2, 3, 4, 5, 6, 7], true, container, "file3.bin");
-        Thread.Sleep(2000);
 
         var files = await _storage.List(container);
         Assert.Equal(3, files.Count);
@@ -149,7 +148,7 @@ public abstract class StorageTestBase
         Assert.InRange(file1.CreatedUtc, file1.UpdatedUtc.AddMilliseconds(-100), file1.UpdatedUtc);
         Assert.Equal(3, file1.SizeBytes);
         var file3 = files.Single(x => x.Path[1] == "file3.bin");
-        Assert.True(file3.UpdatedUtc > file3.CreatedUtc);
+        Assert.True(file3.UpdatedUtc > file3.CreatedUtc, $"{file3.UpdatedUtc} is not greater than {file3.CreatedUtc}, but should be.");
         Assert.Equal(container, file3.Path[0]);
         Assert.Equal("file3.bin", file3.Path[1]);
         Assert.Equal(7, file3.SizeBytes);
