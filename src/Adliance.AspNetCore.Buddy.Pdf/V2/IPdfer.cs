cac6c21b75382ab81347d9b35b2c98b999bbfebe
Alexander Freistetter <alexander.freistetter@adliance.net>
2025-10-31 14:49:31 +0100

    first draft for handlebars
    #12432

--- src/Adliance.AspNetCore.Buddy.Pdf/V2/IPdfer.cs
+++ src/Adliance.AspNetCore.Buddy.Pdf/V2/IPdfer.cs
@@ -5,4 +5,7 @@ namespace Adliance.AspNetCore.Buddy.Pdf.V2;
 public interface IPdfer
 {
     Task<byte[]> HtmlToPdf(string html, PdfOptions options);
+
+    //TODO: separate class for template/options?
+    Task<byte[]> TemplateToPdf(string template, object model, string? js, string? headerTemplate, object? headerModel, string? headerJs, string? footerTemplate, object? footerModel, string? footerJs, PdfOptions options);
 }
