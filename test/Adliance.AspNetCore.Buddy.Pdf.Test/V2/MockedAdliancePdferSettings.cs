86415aa71b90023101c7ed4a0623af3d01190ac2
Hannes Sachsenhofer <hannes.sachsenhofer@adliance.net>
2025-12-08 11:11:49 +0100

    Fix unit tests.

--- test/Adliance.AspNetCore.Buddy.Pdf.Test/V2/MockedAdliancePdferSettings.cs
+++ test/Adliance.AspNetCore.Buddy.Pdf.Test/V2/MockedAdliancePdferSettings.cs
@@ -5,4 +5,6 @@ namespace Adliance.AspNetCore.Buddy.Pdf.Test.V2;
 public class MockedPdferConfiguration : IPdferConfiguration
 {
     public string ServerUrl => "https://pdf2.adliance.dev";
+    public string? ApiKeyPdf => null;
+    public string? ApiKeyTemplate => null;
 }
--- test/Adliance.AspNetCore.Buddy.Pdf.Test/V2/PdferHealthCheckTest.cs
+++ test/Adliance.AspNetCore.Buddy.Pdf.Test/V2/PdferHealthCheckTest.cs
@@ -11,7 +11,7 @@ public class PdferHealthCheckTest
     [Fact]
     public async Task Health_Check_Succeeds()
     {
-        var check = new PdferHealthCheck(new AdliancePdfer(new MockedPdferConfiguration()), new NullLogger<PdferHealthCheck>());
+        var check = new PdferHealthCheck(new MockedPdferConfiguration(), new NullLogger<PdferHealthCheck>());
         Assert.Equal(HealthStatus.Healthy, (await check.CheckHealthAsync(new HealthCheckContext())).Status);
     }
 }
