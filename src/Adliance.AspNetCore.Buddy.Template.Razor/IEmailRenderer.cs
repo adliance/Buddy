af98bb5e80b638ec739a42689fa5e6bd632877f5
Daniel Morawetz <daniel.morawetz@adliance.net>
2025-12-22 15:46:50 +0100

    Add email sending overloads also to email renderer

--- src/Adliance.AspNetCore.Buddy.Template.Razor/IEmailRenderer.cs
+++ src/Adliance.AspNetCore.Buddy.Template.Razor/IEmailRenderer.cs
@@ -9,7 +9,7 @@ namespace Adliance.AspNetCore.Buddy.Template.Razor;
 public interface IEmailRenderer
 {
     /// <summary>
-    /// Renders a several razor templates provided in the "EmailTemplates" directory and sends the result as an email message.
+    /// Renders several razor templates provided in the "EmailTemplates" directory and sends the result as an email message.
     /// </summary>
     /// <param name="recipientAddress">The email address of the recipient of the message.</param>
     /// <param name="templateBaseName">The template's base name.</param>
@@ -17,6 +17,27 @@ public interface IEmailRenderer
     /// <param name="attachments">A collection of file attachments that will be included in the message.</param>
     Task RenderAndSend(string recipientAddress, string templateBaseName, object viewModel,
         params IEmailAttachment[] attachments);
+
+    /// <summary>
+    /// Renders several razor templates provided in the "EmailTemplates" directory and sends the result as an email message.
+    /// </summary>
+    /// <param name="to">The addresses in the To header are the primary recipients of the message.</param>
+    /// <param name="cc">The addresses in the CC header are the secondary recipients of the message, which should receive a copy.</param>
+    /// <param name="bcc">The addresses in the BCC header are secondary recipients of the message, which should receive a copy without information about other recipients.</param>
+    /// <param name="templateBaseName">The template's base name.</param>
+    /// <param name="viewModel">The view model data for the razor template.</param>
+    /// <param name="attachments">A collection of file attachments that will be included in the message.</param>
+    Task RenderAndSend(IEmailRecipient[] to, IEmailRecipient[] cc, IEmailRecipient[] bcc, string templateBaseName,
+        object viewModel, params IEmailAttachment[] attachments);
+
+    /// <summary>
+    /// Renders several razor templates provided in the "EmailTemplates" directory and sends the result as an email message.
+    /// </summary>
+    /// <param name="to">The addresses in the To header are the primary recipients of the message.</param>
+    /// <param name="templateBaseName">The template's base name.</param>
+    /// <param name="viewModel">The view model data for the razor template.</param>
+    /// <param name="attachments">A collection of file attachments that will be included in the message.</param>
+    Task RenderAndSend(IEmailRecipient[] to, string templateBaseName, object viewModel, params IEmailAttachment[] attachments);
 
     /// <summary>
     /// Renders several razor templates provided in the <paramref name="templateDirectoryName"/> directory
@@ -30,5 +51,35 @@ public interface IEmailRenderer
     /// <param name="viewModel">The view model data for the razor template.</param>
     /// <param name="attachments">A collection of file attachments that will be included in the message.</param>
     Task RenderAndSend(string recipientAddress, string templateDirectoryName, string subjectTemplateName,
+        string htmlTemplateName, string textTemplateName, object viewModel, params IEmailAttachment[] attachments);
+
+    /// <summary>
+    /// Renders several razor templates provided in the <paramref name="templateDirectoryName"/> directory
+    /// and sends the result as an email message.
+    /// </summary>
+    /// <param name="to">The addresses in the To header are the primary recipients of the message.</param>
+    /// <param name="cc">The addresses in the CC header are the secondary recipients of the message, which should receive a copy.</param>
+    /// <param name="bcc">The addresses in the BCC header are secondary recipients of the message, which should receive a copy without information about other recipients.</param>
+    /// <param name="templateDirectoryName">The directory the templates are located in.</param>
+    /// <param name="subjectTemplateName">The template's name for the subject of the message.</param>
+    /// <param name="htmlTemplateName">The template's name for the html formatted version of the message body.</param>
+    /// <param name="textTemplateName">The template's name for the plain-text formatted version of the message body.</param>
+    /// <param name="viewModel">The view model data for the razor template.</param>
+    /// <param name="attachments">A collection of file attachments that will be included in the message.</param>
+    Task RenderAndSend(IEmailRecipient[] to, IEmailRecipient[] cc, IEmailRecipient[] bcc, string templateDirectoryName, string subjectTemplateName,
+        string htmlTemplateName, string textTemplateName, object viewModel, params IEmailAttachment[] attachments);
+
+    /// <summary>
+    /// Renders several razor templates provided in the <paramref name="templateDirectoryName"/> directory
+    /// and sends the result as an email message.
+    /// </summary>
+    /// <param name="to">The addresses in the To header are the primary recipients of the message.</param>
+    /// <param name="templateDirectoryName">The directory the templates are located in.</param>
+    /// <param name="subjectTemplateName">The template's name for the subject of the message.</param>
+    /// <param name="htmlTemplateName">The template's name for the html formatted version of the message body.</param>
+    /// <param name="textTemplateName">The template's name for the plain-text formatted version of the message body.</param>
+    /// <param name="viewModel">The view model data for the razor template.</param>
+    /// <param name="attachments">A collection of file attachments that will be included in the message.</param>
+    Task RenderAndSend(IEmailRecipient[] to, string templateDirectoryName, string subjectTemplateName,
         string htmlTemplateName, string textTemplateName, object viewModel, params IEmailAttachment[] attachments);
 }
