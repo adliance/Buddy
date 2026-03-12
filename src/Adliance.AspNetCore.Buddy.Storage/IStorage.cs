4665b97ab5205e6cab75f28e7c45812958bf531a
Hannes Sachsenhofer <hannes.sachsenhofer@adliance.net>
2026-03-12 13:35:20 +0100

    Add overloads for easier handling of IStorageFiles.

--- src/Adliance.AspNetCore.Buddy.Storage/IStorage.cs
+++ src/Adliance.AspNetCore.Buddy.Storage/IStorage.cs
@@ -18,6 +18,11 @@ public interface IStorage
     /// <param name="path">The path of the file to store.</param>
     Task Save(byte[] bytes, bool overwrite = true, params string[] path);
 
+    public async Task Save(byte[] bytes, bool overwrite, IStorageFile file)
+    {
+        await Save(bytes, overwrite, file.Path);
+    }
+
     /// <summary>
     /// Stores a blob file <paramref name="stream"/> with a specified <paramref name="path"/>.
     /// </summary>
@@ -26,6 +31,11 @@ public interface IStorage
     /// <param name="path">The path of the file to store.</param>
     Task Save(Stream stream, bool overwrite = true, params string[] path);
 
+    public async Task Save(Stream stream, bool overwrite, IStorageFile file)
+    {
+        await Save(stream, overwrite, file.Path);
+    }
+
     /// <summary>
     /// Loads a blob file by a specified <paramref name="path"/>.
     /// </summary>
@@ -33,8 +43,24 @@ public interface IStorage
     /// <returns>The loaded blob file.</returns>
     Task<byte[]?> Load(params string[] path);
 
+    public async Task<byte[]?> Load(IStorageFile file)
+    {
+        return await Load(file.Path);
+    }
+
     Task<DateTime?> GetUpdatedUtc(params string[] path);
+
+    public async Task<DateTime?> GetUpdatedUtc(IStorageFile file)
+    {
+        return await GetUpdatedUtc(file.Path);
+    }
+
     Task<DateTime?> GetCreatedUtc(params string[] path);
+
+    public async Task<DateTime?> GetCreatedUtc(IStorageFile file)
+    {
+        return await GetCreatedUtc(file.Path);
+    }
 
     /// <summary>
     /// Loads a blob file by a specified <paramref name="path"/>.
@@ -42,6 +68,11 @@ public interface IStorage
     /// <param name="stream">The stream for the file.</param>
     /// <param name="path">The path of the file to load.</param>
     Task Load(Stream stream, params string[] path);
+
+    public async Task Load(Stream stream, IStorageFile file)
+    {
+        await Load(stream, file.Path);
+    }
 
     /// <summary>
     /// Gets an URL for a file to access it for downloading.
@@ -54,6 +85,11 @@ public interface IStorage
     /// <returns>The URL of the file to download</returns>
     Task<Uri?> GetDownloadUrl(string niceName, DateTimeOffset expiresOn, params string[] path);
 
+    public async Task<Uri?> GetDownloadUrl(string niceName, DateTimeOffset expiresOn, IStorageFile file file)
+    {
+        return await GetDownloadUrl(niceName, expiresOn, file.Path);
+    }
+
     /// <summary>
     /// Determines whether the specified file exists.
     /// </summary>
@@ -62,11 +98,21 @@ public interface IStorage
     /// otherwise, the result is false. Note that if path describes a directory, Exists will return true.</returns>
     Task<bool> Exists(params string[] path);
 
+    public async Task<bool> Exists(IStorageFile file)
+    {
+        return await Exists(file.Path);
+    }
+
     /// <summary>
     /// Deletes the specified file.
     /// </summary>
     /// <param name="path">The path of the file.</param>
     Task Delete(params string[] path);
+
+    public async Task Delete(IStorageFile file)
+    {
+        await Delete(file.Path);
+    }
 
     /// <summary>
     /// Lists all file in the specified container/directory.
