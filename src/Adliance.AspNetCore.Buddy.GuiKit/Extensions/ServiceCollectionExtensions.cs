645d02f7ceeefae3a90892cdfd7186902fbe0a82
Hannes <hannes.sachsenhofer@adliance.net>
2026-06-16 10:51:43 +0200

    Add extension method for easy AppInsights configuration.

---
+++ src/Adliance.AspNetCore.Buddy.GuiKit/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Reflection;
+using Microsoft.ApplicationInsights.AspNetCore.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Adliance.AspNetCore.Buddy.GuiKit.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static void AddApplicationInsightsIfConfigured(this IServiceCollection services)
+    {
+        var connectionString = Environment.GetEnvironmentVariable("APPLICATIONINSIGHTS_CONNECTION_STRING");
+        if (!string.IsNullOrWhiteSpace(connectionString))
+        {
+            var version = Assembly.GetEntryAssembly()?.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion ?? "";
+            Console.WriteLine($"Using Application Insights with app version \"{version}\" and connection string: \"{connectionString}\".");
+            services.AddApplicationInsightsTelemetry(new ApplicationInsightsServiceOptions
+            {
+                ConnectionString = connectionString,
+                ApplicationVersion = version
+            });
+        }
+        else
+        {
+            Console.WriteLine("No Application Insights connection string configured.");
+        }
+    }
+}
